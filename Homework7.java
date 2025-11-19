2e33967b06a97ce02f87f777f875251268ce9581
Ham Jiwon <97371009+CodeHJW@users.noreply.github.com>
2025-11-19 22:56:06 +0900

    Add files via upload

---
+++ Homework7.java
@@ -0,0 +1,53 @@
+abstract class Printer{
+    String model;
+    int printedCount;
+    int availableCount;
+    Printer(String model, int availableCount) {
+        this.model = model;
+        this.availableCount = availableCount;
+        printedCount = 0;
+    }
+    abstract boolean print();
+}
+class InkjetPrinter extends Printer{
+    InkjetPrinter(String model, int availableCount) {
+        super(model, availableCount);
+    }
+    @Override
+    boolean print(){
+        if(availableCount == 0){
+            System.out.println(model+": "+(printedCount+1)+"매째 인쇄 실패 - 잉크 부족.");
+            return false;
+        }else {
+            availableCount--;
+            printedCount++;
+            return true;
+        }
+
+    }
+}
+class LaserPrinter extends Printer{
+    LaserPrinter(String model, int availableCount) {
+        super(model, availableCount);
+    }
+    @Override
+    boolean print(){
+        if(availableCount == 0){
+            System.out.println(model+": "+(printedCount+1)+"매째 인쇄 실패 - 토너 부족.");
+            return false;
+        }else {
+            availableCount--;
+            printedCount++;
+            return true;
+        }
+    }
+}
+
+public class Homework7 {
+    public static void main(String[] args) {
+        InkjetPrinter inkjet = new InkjetPrinter("Brother DCP-T730DW", 7500);
+        LaserPrinter laser = new LaserPrinter("Canon MFC643Cdw", 1500);
+        while (inkjet.print());
+        while (laser.print());
+    }
+}
